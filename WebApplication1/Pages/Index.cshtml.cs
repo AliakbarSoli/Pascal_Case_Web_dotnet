e22d9cc67eb9b88b06c8cef85547c8fad3c61c09
Aliakbar Soltani <aliakbarsoltaniiiii@gmail.com>
2023-11-19 13:03:22 +0330

    Add project files.

---
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+using System.Text;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string P_Case => (string)TempData[nameof(P_Case)];
+        TextInfo textInfo = new CultureInfo("en-US",false).TextInfo;
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public IActionResult OnPost([FromForm] string pcase)
+        {
+            TempData["Name"] = textInfo.ToTitleCase(pcase);
+            //TempData["Name"] = name;
+            return RedirectToPage("Index");
+
+                
+        }
+    }
+}
